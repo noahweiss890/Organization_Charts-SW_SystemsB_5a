cfcbbbbb4fa03a9d404302050b7a81b517f33b1d
noahweiss890 <nweiss890@gmail.com>
2022-05-16 01:32:42 +0300

    adding test

---
+++ Test.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdexcept>
+#include "doctest.h"
+#include "sources/OrgChart.hpp"
+
+using namespace std;
+using namespace ariel;
+
+TEST_CASE("testing 123") {
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+}
